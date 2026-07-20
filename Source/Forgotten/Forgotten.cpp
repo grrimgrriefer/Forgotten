8980f6966983a94a81506ba6f66ff09095347e05
grrimgrriefer <griefygrimmer@gmail.com>
2026-07-20 20:24:27 +0200

    clean empty UE5.8 project

---
+++ Source/Forgotten/Forgotten.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Forgotten.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Forgotten, "Forgotten" );
