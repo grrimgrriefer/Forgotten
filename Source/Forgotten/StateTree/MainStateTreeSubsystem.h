24b8e84726344b9c2f51f8489aa960af942dec2d
grrimgrriefer <168567347+grrimgrriefer@users.noreply.github.com>
2026-07-23 07:06:01 +0200

    Moved main statetree handling over to subsystem

---
+++ Source/Forgotten/StateTree/MainStateTreeSubsystem.h
@@ -0,0 +1,53 @@
+// Copyright(c) 2026 grrimgrriefer & DZnnah, see LICENSE for details.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tickable.h"
+#include "StateTreeInstanceData.h"
+#include "Engine/World.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MainStateTreeSubsystem.generated.h"
+
+class UStateTree;
+
+/**
+ * Holds the main StateTree and controls the high-level flow of the game.
+ */
+UCLASS(Abstract, Blueprintable)
+class FORGOTTEN_API UMainStateTreeSubsystem : public UGameInstanceSubsystem, public FTickableGameObject
+{
+	GENERATED_BODY()
+	
+public:	
+	#pragma region UGameInstanceSubsystem
+		virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+		virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+		virtual void Deinitialize() override;
+	#pragma endregion UGameInstanceSubsystem
+
+	#pragma region FTickableGameObject
+		virtual void Tick(const float deltaTime) override;
+		virtual ETickableTickType GetTickableTickType() const override;
+		virtual TStatId GetStatId() const override;
+		virtual bool IsTickableWhenPaused() const override;
+		virtual bool IsTickableInEditor() const override;
+		virtual bool IsTickable() const override;
+	#pragma endregion FTickableGameObject
+
+	UFUNCTION(BlueprintCallable, Category = "GameFlow")
+	void SendFlowEvent(const FGameplayTag tag);
+	 
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Settings")
+	TObjectPtr<UStateTree> m_stateTreeAsset;
+	 
+private:
+	void OnPostWorldInitialization(UWorld* world, const UWorld::InitializationValues iValues);
+
+	UPROPERTY()
+	FStateTreeInstanceData m_instanceData;
+	
+	uint32 m_lastFrameNumberWeTicked = INDEX_NONE;
+	bool m_isRunning = false;
+};
