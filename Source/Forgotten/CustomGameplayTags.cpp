9144f7b9d03b56329a9a785284ea6b3c28d9ed11
grrimgrriefer <168567347+grrimgrriefer@users.noreply.github.com>
2026-08-14 13:03:03 +0200

    Added very basic conversation UI & option to exit conversations

--- Source/Forgotten/CustomGameplayTags.cpp
+++ Source/Forgotten/CustomGameplayTags.cpp
@@ -1,3 +1,6 @@
+// Copyright(c) 2026 grrimgrriefer & DZnnah, see LICENSE for details.
+
 #include "CustomGameplayTags.h"
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_State_Conversation_Start, "State.Conversation.Start");
+UE_DEFINE_GAMEPLAY_TAG(TAG_State_Conversation_End, "State.Conversation.End");
--- Source/Forgotten/StateTree/Tasks/ConversationTask.h
+++ Source/Forgotten/StateTree/Tasks/ConversationTask.h
@@ -4,10 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "StateTreeTaskBase.h"
+#include "Templates/SubclassOf.h"
 #include "ConversationTask.generated.h"
 
 class AFirstPersonCharacter;
 class ARainNPC;
+class UConversationWidget;
 
 USTRUCT()
 struct FConversationTaskInstanceData
@@ -18,10 +20,15 @@ struct FConversationTaskInstanceData
 	TWeakObjectPtr<AFirstPersonCharacter> m_PlayerCharacter = nullptr;
 	UPROPERTY(Transient)
 	TWeakObjectPtr<ARainNPC> m_TargetNPC = nullptr;
+	UPROPERTY(Transient)
+	TWeakObjectPtr<UConversationWidget> m_WidgetPtr = nullptr;
+
+	FDelegateHandle m_TextSubmittedHandle;
+	FDelegateHandle m_EndedHandle;
 };
 
 /**
- * Tells the player to lock movement and change camera orientation during NPC conversation.
+ * Tells the player to lock movement, spawn conversation UI, and change camera orientation.
  */
 USTRUCT(meta = (DisplayName = "Conversation State", Category = "Gameplay"))
 struct FORGOTTEN_API FConversationTask : public FStateTreeTaskBase
@@ -40,4 +47,7 @@ public:
 	virtual void ExitState(
 		FStateTreeExecutionContext& context,
 		const FStateTreeTransitionResult& transitions) const override;
+
+	UPROPERTY(EditAnywhere, Category = "UI")
+	TSubclassOf<UConversationWidget> m_ConversationWidgetClass;
 };
